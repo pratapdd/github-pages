a5a219a94c4fe97a88d613f0b64f0a26fe5ffa78
pratapdd <42590130+pratapdd@users.noreply.github.com>
2020-11-15 07:07:47 +0530

    Update playground.js

--- docs/playground.js
+++ docs/playground.js
@@ -14,14 +14,23 @@ function replaceWith() {
 }
 
 function replaceWithReset() {
-    kQuery('.second_marker').after('<div class="second_item">Some</div><div class="second_item">Item</div>')
+    kQuery('.second_marker').after('<div class="second_item">Some</div><div class="second_item">Item</div>');
 }
 
 
-function style() {
-    kQuery('.third_item').style({backgroundColor: 'grey'})
+function setStyle() {
+    kQuery('.third_item').style({backgroundColor: 'grey'});
 }
 
 function styleReset() {
     kQuery('.third_item').style({backgroundColor: 'transparent'});
+}
+
+
+function remove() {
+    kQuery('.fourth_item').remove();
+}
+
+function removeReset() {
+    kQuery('.fourth_marker').after('<div class="fourth_item">Some</div><div class="fourth_item">Item</div>');
 }
