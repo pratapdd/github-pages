03810f54f2711a9f0c70015f403d846ae4b5eb2e
pratapdd <42590130+pratapdd@users.noreply.github.com>
2020-11-15 07:30:16 +0530

    Update playground.js

--- docs/playground.js
+++ docs/playground.js
@@ -34,3 +34,35 @@ function remove() {
 function removeReset() {
     kQuery('.fourth_marker').after('<div class="fourth_item">Some</div><div class="fourth_item">Item</div>');
 }
+
+let fifthSpan;
+function find() {
+    fifthSpan = kQuery('.fifth_item').find('span');
+}
+
+function findReset() {
+    console.log('reset')
+    console.log(fifthSpan);
+    if (fifthSpan) {
+        fifthSpan.remove();
+    }
+    kQuery('fifth_marker').after('<div class="fifth_item">Hello <span>World!</span></div><div class="fifth_item">World! <span>Hello</span></div>');
+}
+
+
+function before() {
+    kQuery('.sixth_inner').before('<p class="sixth_extra">Text</p>');
+}
+
+function beforeReset() {
+    kQuery('.sixth_extra').remove();
+}
+
+
+function after() {
+    kQuery('.seventh_inner').after('<p class="seventh_extra">Text</p>');
+}
+
+function afterReset() {
+    kQuery('.seventh_extra').remove();
+}
